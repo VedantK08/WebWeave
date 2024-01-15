e9c0cebf53f8a388e1ece543fabe784b9698942e
VedantK08 <vedant.kulkarni777@gmail.com>
2024-01-15 22:02:07 +0530

    Changed codebase

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,21 @@ import Pages from "./Pages/Pages";
 import LoginPage from "./Pages/LoginPage";
 import Account from "./Pages/Account";
 import SignUp from "./Pages/SignUp";
+import Root from "./Root";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/userHome" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/pages" element={<Pages />} />
-        <Route path="/userLogin" element={<LoginPage />} />
-        <Route path="/myAccount" element={<Account />} />
+        <Route path="/" element={<Root />}>
+          <Route index element={<LoginPage />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/userHome" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/pages" element={<Pages />} />
+          <Route path="/userLogin" element={<LoginPage />} />
+          <Route path="/myAccount" element={<Account />} />
+        </Route>
       </Routes>
     </Router>
   );
